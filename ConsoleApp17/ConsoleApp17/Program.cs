7a2ecd773fa028045293521d2a5f05fc0b5047f4
afonaizer <123896092+afonaizer@users.noreply.github.com>
2023-10-25 21:01:00 +0300

    Add files via upload

---
+++ ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            StringBuilder s = new StringBuilder(Console.ReadLine());
+            StringBuilder s1 = s.Replace("х", string.Empty);
+            Console.Write($"Строка без х: {s1}");
+            Console.ReadKey();
+
+        }
+    }
+}
